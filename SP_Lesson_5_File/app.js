1e33901f1a612deba11786109a398a6b35855a0a
vasen <vasenking@mail.ru>
2019-03-02 19:11:03 +0500

    Save file, and write Number information

--- SP_Lesson_5_File/app.js
+++ SP_Lesson_5_File/app.js
@@ -3,7 +3,6 @@ const app = express();
 const upload = require('express-fileupload');
 const handlerFunc = require('./handler');
 const bodyParser = require('body-parser');
-const fs = require('fs');
 
 app.use(bodyParser.json());
 app.use(upload());
@@ -14,39 +13,15 @@ app.get('/', (req, res) => {
 
 app.post("/handle/text/", (req, res) => {
     handlerFunc(JSON.stringify(req.body.text));
-    res.send(req.body);
+    res.redirect('/');
 });
 
 app.post('/handle/file/', (req, res) => {
-    if (req.files.upfile) {
-        var file = req.files.upfile,
-            name = file.name;
-        var uploadpath = __dirname + '/uploads/' + name;
-        file.mv(uploadpath, function (err) {
-            if (err) {
-                console.log("File Upload Failed", name, err);
-                res.send("Error Occured!")
-            }
-            else {
-                console.log("File Uploaded", name);
-                res.send('Done! Uploading files')
-            }
-        });
-    }
-    else {
-        res.send("No File selected !");
-        res.end();
-    }
+    if (req.files.upfile) handlerFunc(String(req.files.upfile.data));
+    res.redirect('/');
 });
 
 app.listen(80, function () {
     console.log('Start Port:80');
 });
 
-
-// let file_read = fs.readFileSync('t1.txt', 'utf-8');
-// const message = "Hello World" + file_read;
-// fs.writeFileSync('some_new_file.txt', message);
-// fs.readFile('t1.txt', {encoding: 'urf-8'}, function (err, data) {
-//     if(err) throw err;
-// });
